0924cdd3f12947c34080e434945e2ae8ad0293fb
sergey_iagfarov <dedaz27@mail.ru>
2022-09-24 22:24:55 +0500

    домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
        float meet = 27.12f;
        long rabbitMeet = 987678965549L;
+       float wolfMeet = 2.786f;
+       boolean heLive = false;
+       short sugar = 569;
+       short solt = -159;
+       short pepper = 27897;
+       byte biff = 67;
 
     }
 }
