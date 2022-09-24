2005ba0697ca51bebe8849c9065c19b2ffe8cf10
sergey_iagfarov <dedaz27@mail.ru>
2022-09-24 21:59:19 +0500

    домашняя работа

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int cat = 5;
+        System.out.println(cat);
+        byte dog = 3;
+        System.out.println(dog);
+        short bag = 100;
+        System.out.println(bag);
+        long blackStone = 1000L;
+        System.out.println(blackStone);
+        float redStone = 3.5f;
+        System.out.println(redStone);
+        double whiteStone = 35.28;
+        System.out.println(whiteStone);
+    }
+}
