07d069274b57b73220a68963f6db52e2ff44fd8a
sergey_iagfarov <dedaz27@mail.ru>
2022-09-25 16:52:52 +0500

    домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int cat = 5;
-        System.out.println(cat);
-        byte dog = 3;
-        System.out.println(dog);
-        short bag = 100;
-        System.out.println(bag);
-        long blackStone = 1000L;
-        System.out.println(blackStone);
-        float redStone = 3.5f;
-        System.out.println(redStone);
-        double whiteStone = 35.28;
-        System.out.println(whiteStone);
+        // Задача 5
+        int totalPaint = 120;
+        System.out.println(totalPaint);
+        int white = 1;
+        System.out.println(white);
+        int brown = 1;
+        System.out.println(brown);
+        int paintForOneClass = (2 * white) + (4 * brown);
+        System.out.println(paintForOneClass);
+        int totalClass = 120 / paintForOneClass;
+        System.out.println("Всего классов " + totalClass);
+        int totalWhitePaint = totalClass * 2;
+        System.out.println(totalWhitePaint);
+        int totalBrownPaint = totalClass * 4;
+        System.out.println(totalBrownPaint);
+        System.out.println("В школе, где " + totalClass + " классов, нужно " + totalWhitePaint + " банок белой краски и " + totalBrownPaint + " банок коричневой краски" );
     }
 }
