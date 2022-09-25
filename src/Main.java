9a5825306efbdcf8a208b218562df68ab151c25e
sergey_iagfarov <dedaz27@mail.ru>
2022-09-25 15:48:09 +0500

    домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int cat = 5;
-        System.out.println(cat);
-        byte dog = 3;
-        System.out.println(dog);
-        short bag = 100;
-        System.out.println(bag);
-        long blackStone = 1000L;
-        System.out.println(blackStone);
-        float redStone = 3.5f;
-        System.out.println(redStone);
-        double whiteStone = 35.28;
-        System.out.println(whiteStone);
+      // Задача 3
+        int lubPav = 23;
+        System.out.println(lubPav);
+        int annSerg = 27;
+        System.out.println(annSerg);
+        int ekAnd = 30;
+        System.out.println(ekAnd);
+        int totalPaper = 480;
+        System.out.println(totalPaper);
+        int totalStudent = lubPav + annSerg + ekAnd;
+        System.out.println("Всего школьников " + totalStudent);
+        int sheetPerOneStudent = totalPaper / totalStudent;
+        System.out.println("На каждого школьника рассчитано " + sheetPerOneStudent + " листов бумаги");
+
+
     }
 }
